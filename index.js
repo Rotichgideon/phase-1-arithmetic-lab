d29e1362e4c3c225dadb8df4644ab6d5319e0f65
gideon rotich <gideonrotich2001@gmail.com>
2024-04-04 22:52:51 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = math.cell(math.random() * 100);
+
+const num3 = 64;
+const num4 =60;
+let mod = num3 % num4;
+
+let max = math.max(1, 3, 5, 20, 15);
+
+module.exports = {
+    multiply,
+    random,
+    mod,
+    max,
+
+};
